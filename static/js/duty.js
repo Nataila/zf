658633887eb91348450d1ab7b826e2a3a94b680d
Nataila <ccfive814@gmail.com>
2016-08-16 06:24:35 +0800

    添加表格

--- static/js/duty.js
+++ static/js/duty.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  // 下拉框
   $('.pull-down').on('click', function () {
     $(this).find('i.pulldown').toggleClass('pullup');
     $('.options').slideToggle();
@@ -7,4 +8,11 @@ $(document).ready(function () {
     $('.year-input').text($(this).text());
     $('.options').slideToggle();
   });
+
+  // tab切换
+  $('.nav-btn-groups li').on('click', function () {
+    $('div.content > div').hide();
+    var content_name = $(this).data('content');
+    $('div#' + content_name).show();
+  })
 });
