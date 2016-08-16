3d3facd22142beb57c05e10d56b5b8058a352acf
Nataila <ccfive814@gmail.com>
2016-08-16 16:01:19 +0800

    update

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
     var spansWidth = ($('.slide-container .slide-content span').width() + 25) * $('.slide-container .slide-content span').length;
     var contentWidth = $('.slide-container .middle').width();
     var leftLen = parseInt($('.slide-container .slide-content').css('left').split('px'));
-    console.log(leftLen);
     if (leftLen === 0) {
       return false
     }
@@ -14,7 +13,7 @@ $(document).ready(function () {
     $('.slide-container .slide-content').animate({'left': spansWidthNew});
   });
   $('.slide-container .right').on('click', function () {
-    var spansWidth = ($('.slide-container .slide-content span').width() + 25) * $('.slide-container .slide-content span').length;
+    var spansWidth = ($('.slide-container .slide-content > span').width() + 25) * $('.slide-container .slide-content > span').length;
     var contentWidth = $('.slide-container .middle').width();
     var leftLen = parseInt($('.slide-container .slide-content').css('left').split('px'));
     var spansWidthNew = leftLen - contentWidth;
@@ -24,7 +23,6 @@ $(document).ready(function () {
     $('.slide-container .slide-content').animate({'left': spansWidthNew});
   });
   $('.slide-container .slide-content span').on('click', function () {
-    console.log(123);
     var modalName = $(this).closest('.slide-content').data('modal');
     $('#' + modalName).show();
   })
